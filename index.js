71ac36343162e2465460a4f56f15a6491c1b1aa9
Vishal Vasan <vishvasan@gmail.com>
2017-05-16 13:37:32 -0400

    Add input string to display and to list

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const items = {}
 const list = document.getElementById('demo');
 
-function changeText2() {
+function changeText() {
     const personName = document.getElementById('personName').value;
-    document.getElementById('boldStuff2').innerHTML = personName;
+    document.getElementById('boldStuff').innerHTML = personName;
     const entry = document.createElement('li');
     entry.appendChild(document.createTextNode(personName));
     list.appendChild(entry);
