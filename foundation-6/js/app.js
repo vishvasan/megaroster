e1c478e68883eadab8e05bad25009eb6f37e81b3
Vishal Vasan <vishvasan@gmail.com>
2017-05-17 11:04:17 -0400

    Create a student object with an ID

--- foundation-6/js/app.js
+++ foundation-6/js/app.js
@@ -1,15 +1,29 @@
 $(document).foundation()
 
 const megaroster = {
-    init() {
-        document.querySelector('#new-student')
-        .addEventListener('submit', this.addStudent)
+    students:[],
+
+    init(listSelector) {
+        this.studentList = document.querySelector('#new-student')
+        this.max = 0
+        document 
+            .querySelector('#new-student')
+            .addEventListener('submit', this.addStudent.bind(this))
     },
 
     addStudent(ev) {
         ev.preventDefault()
-        const studentName = ev.target.studentName.value
-        console.log(studentName)
+        const f = ev.target
+        const student = {
+            id: this.max + 1,
+            name: f.studentName.value,
+        }
+        this.buildListItem(student)
+        this.max++
+    },
+
+    buildListItem(student) {
+
     },
 }
-megaroster.init()
+megaroster.init('#studentList')
